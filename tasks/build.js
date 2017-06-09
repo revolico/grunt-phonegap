83d380fb59bc5207588a77b7272350eca93dfd16
cundejo <cundejo85@gmail.com>
2017-06-09 11:40:42 -0400

    Removed the task cloneCordova to copy the folder .cordova inside of the cordova project. In fact this folder is unnecessary in cordova.

--- tasks/build.js
+++ tasks/build.js
@@ -11,7 +11,7 @@
       cloneRoot: require('./build/base/clone_root')(grunt).run,
       cloneMerges: require('./build/base/clone_merges')(grunt).run,
       indexHtml: require('./build/base/index_html')(grunt).run,
-      cloneCordova: require('./build/base/clone_cordova')(grunt).run,
+      // cloneCordova: require('./build/base/clone_cordova')(grunt).run,
       compileConfig: require('./build/base/compile_config')(grunt).run,
       copyConfig: require('./build/base/copy_config')(grunt).run,
       addPlugins: require('./build/base/plugin')(grunt).add,
@@ -23,7 +23,7 @@
     return {
       run: function(platforms, fn) {
         platforms = helpers.reducePlatforms(platforms);
-        return fluid(base).clean().createTree(platforms).cloneRoot().cloneMerges().indexHtml().cloneCordova().compileConfig().copyConfig().custom(function(done) {
+        return fluid(base).clean().createTree(platforms).cloneRoot().cloneMerges().indexHtml().compileConfig().copyConfig().custom(function(done) {
           return base.addPlugins(plugins, function() {
             return done();
           });
